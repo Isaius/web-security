c7354b5c10e4f06035f8254d11c7b27ed540f5ce
alvaro-ian <alvaro-ian@hotmail.com>
2020-09-19 17:46:40 -0300

    Add comment controls

---
+++ src/controllers/CommentController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express"
+import db from "../database/connection"
+
+class CommentController {
+    async index(req: Request, res: Response){
+        console.log("Recuperando comentários");
+
+        const comments = await db.select().table('comments');
+
+        return res.json(comments);
+    }
+
+    async store(req: Request, res: Response){
+        const {
+            user_id,
+            content
+        } = req.body;
+
+        console.log("Novo comentário de " + user_id + ": " + content);
+
+        await db('comments').insert({
+            content,
+            user_id
+        });
+
+        console.log("Comentário salvo");
+        return res.sendStatus(200);
+    }
+}
+
+export default CommentController
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-
+import CommentController from './controllers/CommentController'
 import UserController from './controllers/UserController'
 
 const routes = express.Router()
@@ -10,5 +10,8 @@ routes.use(express.static(path.join(__dirname, 'public')))
 routes.get('/users', new UserController().index)
 routes.post('/users', new UserController().store)
 routes.delete('/users', new UserController().delete)
+
+routes.get('/comments', new CommentController().index)
+routes.post('/comments', new CommentController().store)
 
 export default routes
