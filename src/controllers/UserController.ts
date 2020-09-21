2b6d812f093b5d42a9fa97575bfabdbab317b3ad
Isaias <isaigrec@gmail.com>
2020-09-21 12:43:49 -0300

    unsafe sql query test with sqlite3

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express"
 import db from "../database/connection"
+import path from 'path'
+
+var sqlite3 = require('sqlite3').verbose();
+var db3 = new sqlite3.Database(path.resolve(__dirname, '..', 'database', 'database.sqlite'))
 
 class UserController {
     async index(req: Request, res: Response){
@@ -9,10 +13,16 @@ class UserController {
         } = req.body;
 
         console.log("Log in: " + username + ", pass: " + password);
+        
+        let user:any = []
 
-        const user = await db.raw("select * from users where username='" + username + "' and password='" + password + "'");
-
-        return res.json(user);
+        await db3.serialize(async function() {
+            await db3.all("select * from users where username='" + username + "' and password='" + password + "'", function(err: any, row: any){
+                user = row
+                console.log(user)
+                return res.json(user);
+            })
+        })    
     }
 
     async store(req: Request, res: Response){
